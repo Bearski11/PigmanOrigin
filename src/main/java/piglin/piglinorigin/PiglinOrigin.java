537f35f379165691e02abb63de62a6771c59c924
BearskiDev <69278707+Bearski11@users.noreply.github.com>
2021-04-18 11:05:44 -0700

    First Commit

---
+++ src/main/java/piglin/piglinorigin/PiglinOrigin.java
@@ -0,0 +1,10 @@
+package piglin.piglinorigin;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PiglinOrigin implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/piglin/piglinorigin/client/PiglinOriginClient.java
@@ -0,0 +1,13 @@
+package piglin.piglinorigin.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class PiglinOriginClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
